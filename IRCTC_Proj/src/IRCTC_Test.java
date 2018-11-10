5966818ca46566eace94a0452c7f059a95e41092
pradip <pradip@gmail.com>
2018-11-10 20:44:56 +0530

    edited bydev smruti

--- IRCTC_Proj/src/IRCTC_Test.java
+++ IRCTC_Proj/src/IRCTC_Test.java
@@ -4,7 +4,7 @@ public class IRCTC_Test {
 	public static void main(String[] args) {
 		System.out.println("IRCTC project created by TL");
 		System.out.println("IRCTC project edited in GIT Central Repo....");
-
+		System.out.println("edited by dev-smruti");
 	}
 
 }
